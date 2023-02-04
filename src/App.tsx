80c7280a9bfe6c7ca66211c94087c6a66d5cf05c
AnkitAegis <ankit@aegiscovenant.com>
2023-02-04 18:29:25 +0000

    App init
    Get started

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import InputFields from 'components/InputFields';
-import { Todo } from 'Model';
+//import { Todo } from 'Model';
 import React, {useState} from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  const [todo,setTodo] = useState<string>("");
-  const [data,setData] = useState<Todo[]>([]);
+  // const [todo,setTodo] = useState<string>("");
+  // const [data,setData] = useState<Todo[]>([]);
 
-  const handleAdd = (e: React.FormEvent)=>{
-    e.preventDefault();
+  // const handleAdd = (e: React.FormEvent)=>{
+  //   e.preventDefault();
 
-    let id:number = Math.random()*100;
-    if(todo==""){
-      return alert("Enter a todo");
-    }
-    let temp = {id,todo,isDone:false};
-    setData([...data,temp]);
-    setTodo("");
-  }
+  //   let id:number = Math.random()*100;
+  //   if(todo==""){
+  //     return alert("Enter a todo");
+  //   }
+  //   let temp = {id,todo,isDone:false};
+  //   setData([...data,temp]);
+  //   setTodo("");
+  // }
   return (
     <div className="App">
-      <header>TaskFolk</header>
-      <InputFields todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <header>CKYC</header>
     </div>
   );
 }
